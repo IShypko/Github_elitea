82fd6fb17fbcbd9b6eaa694eac9cc08f45f97c9f
IShypko <77001747+IShypko@users.noreply.github.com>
2026-03-13 19:48:34 +0100

    Create tests/client-work.spec.js

---
+++ tests/client-work.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+// Skeleton: test steps will be added in a follow-up edit to keep changes incremental
+test('Verify Client Work text is visible on EPAM site - skeleton', async ({ page }) => {
+  // TODO: add navigation and assertions
+});
