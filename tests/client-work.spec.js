77336b8fced3f261b91366bd77cda4441bf8d5eb
IShypko <77001747+IShypko@users.noreply.github.com>
2026-03-13 19:49:01 +0100

    Delete tests/client-work.spec.js

--- tests/client-work.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// Skeleton: test steps will be added in a follow-up edit to keep changes incremental
-test('Verify Client Work text is visible on EPAM site - skeleton', async ({ page }) => {
-  // TODO: add navigation and assertions
-});
